ba4c7c4b4941674b2729fe5cf4f1d3e9d8e776a4
Yao <yaozhou.wuhu@gmail.com>
2017-02-18 15:24:18 +0800

    添加第一版代码

--- index.android.js
+++ index.android.js
@@ -1,53 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// to
+// fetch cookie  处理
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Alert, DrawerLayoutAndroid, Navigator, AppState} from 'react-native';
+import { Container, Header, Title, Content , Button , Icon, List, ListItem, InputGroup, Input} from 'native-base'; 
+import ModalDropdown from 'react-native-modal-dropdown' ;
+import Moment from 'moment' ;
+import Range from 'range'
+import Options from './components/Options.js'
+import Appointment from './components/Appointment'
+import Main from './components/Main'
+import yihu from './yihu'
+import CacheStore from 'react-native-cache-store';
+//import codePush from "react-native-code-push";
+import config from './config.json' ;
 
-export default class yuyue extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+// am start -n com.hospital/.MainActivity
+ // am startservice -a com.hospital.headless -e jobKey yao
+ // adb logcat hospital:V | egrep "hospital|ReactNativeJS|PowerManagerFlow"
+
+class yuyue extends Component {
+    constructor (props) {
+        super(props);      
+    }
+
+   render() {
+            
+            return (
+                    <Navigator
+                      initialRoute={{ name: 'Main', component: Main }}
+                      configureScene={(route) => {
+                        return Navigator.SceneConfigs.PushFromRight ;
+                      }}
+                      renderScene={(route, navigator) => {
+                        let Component = route.component ;
+                        return <Component {...route.params} navigator={navigator} />
+                      }} />
+            );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+//hospital = codePush({deploymentKey : config.staging_key})(hospital) ;
 
 AppRegistry.registerComponent('yuyue', () => yuyue);
+
+module.exports = yuyue ;
